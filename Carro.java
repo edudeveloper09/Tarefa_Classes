ff058ed42e4de3c711ab9890fd9574e696f20459
edudeveloper09 <157771672+edudeveloper09@users.noreply.github.com>
2024-03-28 20:28:07 -0300

    Add files via upload

---
+++ Carro.java
@@ -0,0 +1,84 @@
+public class Carro {
+
+//Este comentário especifíca a classe do objeto carro e seus atributos
+    private int codigo;
+
+    private String marca;
+
+    private String modelo;
+
+    private int ano;
+
+    private String cor;
+
+    private int quantidadePortas;
+
+    private int quilometragem;
+
+    private int preco;
+
+ //Métodos.
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getQuantidadePortas() {
+        return quantidadePortas;
+    }
+
+    public void setQuantidadePortas(int quantidadePortas) {
+        this.quantidadePortas = quantidadePortas;
+    }
+
+    public int getQuilometragem() {
+        return quilometragem;
+    }
+
+    public void setQuilometragem(int quilometragem) {
+        this.quilometragem = quilometragem;
+    }
+
+    public int getPreco() {
+        return preco;
+    }
+
+    public void setPreco(int preco) {
+        this.preco = preco;
+    }
+}
